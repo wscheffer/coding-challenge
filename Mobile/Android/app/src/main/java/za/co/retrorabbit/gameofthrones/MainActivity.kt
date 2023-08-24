193e50d0c6433228d85f70731e1fdc36df3ecb84
Henri Langenhoven <hlangenhoven@retrorabbit.co.za>
2023-08-24 13:09:21 +0200

    Code cleanup

--- Mobile/Android/app/src/main/java/za/co/retrorabbit/gameofthrones/MainActivity.kt
+++ Mobile/Android/app/src/main/java/za/co/retrorabbit/gameofthrones/MainActivity.kt
@@ -1,21 +1,16 @@
-@file:OptIn(ExperimentalMaterial3Api::class)
-
 package za.co.retrorabbit.gameofthrones
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import za.co.retrorabbit.gameofthrones.router.Router
 import za.co.retrorabbit.gameofthrones.ui.theme.GameOfThronesTheme
-
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
 
         setContent {
             GameOfThronesTheme {
@@ -24,14 +19,8 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-
-    @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     fun MainScreen() {
         Router()
     }
 }
-
-
-
-
